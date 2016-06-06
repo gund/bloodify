a4cf7e8b852bf4183dfaaa964a4f586e8dd363b4
Alex Malkevich <malkevich.alex@gmail.com>
2016-06-06 22:34:03 +0400

    Move work with map from Map Component to Map Service Update map view styles

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,29 +7,18 @@ import {
 } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
-const AGS_MAP = require('esri/Map');
-const AGS_MAP_VIEW = require('esri/views/MapView');
-const AGS_GRAPH_LAYER = require('esri/layers/GraphicsLayer');
-const AGS_LOCATE_WID = require('esri/widgets/Locate');
-const AGS_SEARCH_WID = require('esri/widgets/Search');
+import { MapService } from './map.service';
 
 const MAP_CLASS = 'bdfy-map';
 
 @Component({
   selector: 'bdfy-map',
-  providers: [],
+  providers: [MapService], // MapService instance per component
   template: `<div id="{{ mapId }}" class="${MAP_CLASS}"></div>`
 })
 export class MapComponent<Pins> implements OnInit, AfterViewInit {
-  static mapCount = 0;
+  static mapsCount = 0;
 
-  @Input() basemap = 'streets';
-  @Input() maxZoom = 20;
-  @Input() minZoom = 2;
-  @Input() locateEnabled = true;
-  @Input() locatePosition = 'top-left';
-  @Input() searchEnabled = true;
-  @Input() searchPosition = 'top-left';
   @Input() pins: Observable<Pins>;
   @Output() pinSelected = new EventEmitter<Pins>();
   @Output() positionChanged = new EventEmitter();
@@ -41,69 +30,18 @@ export class MapComponent<Pins> implements OnInit, AfterViewInit {
   locateBtn: __esri.Locate;
   searchBox: __esri.Search;
 
-  constructor() {
-    this.mapId = MAP_CLASS + (MapComponent.mapCount++).toString();
+  constructor(protected mapService: MapService) {
+    this.mapId = MAP_CLASS + (MapComponent.mapsCount++).toString();
   }
 
   ngOnInit(): any {
   }
 
   ngAfterViewInit(): any {
-    this.initMap();
-    this.initView();
-    this.initGraphicsLayer();
-    if (this.locateEnabled) this.initLocateBtn();
-    if (this.searchEnabled) this.initSearchBox();
-  }
-
-  viewDidInit() {
-    if (this.locateEnabled) this.locateBtn.locate();
-  }
-
-  initMap() {
-    this.map = new AGS_MAP({
-      basemap: this.basemap
-    });
-  }
-
-  initView() {
-    this.view = new AGS_MAP_VIEW({
-      container: this.mapId,
-      map: this.map,
-      constraints: {
-        maxZoom: this.maxZoom,
-        minZoom: this.minZoom,
-        rotationEnabled: false
-      }
-    });
-
-    this.view.then(() => this.viewDidInit());
-  }
-
-  initGraphicsLayer() {
-    this.graphicsLayer = new AGS_GRAPH_LAYER();
-    this.map.add(this.graphicsLayer);
-  }
-
-  initLocateBtn() {
-    this.locateBtn = new AGS_LOCATE_WID({
-      view: this.view,
-      graphicsLayer: this.graphicsLayer
-    });
-    this.locateBtn.startup();
-    this.view.ui.add(this.locateBtn, {
-      position: this.locatePosition,
-      index: 1
-    });
-  }
-
-  initSearchBox() {
-    this.searchBox = new AGS_SEARCH_WID({
-      view: this.view
-    });
-    this.view.ui.add(this.searchBox, {
-      position: this.searchPosition,
-      index: 0
-    });
+    this.mapService
+      .initialize({container: this.mapId})
+      .withSearchBox()
+      .withLocateBtn()
+      .locateOnInit();
   }
 }
---
+++ src/app/map/map.service.ts
@@ -0,0 +1,99 @@
+/**
+ * Created by alex on 6/6/16.
+ */
+
+import { Injectable } from '@angular/core';
+
+const AGS_MAP = require('esri/Map');
+const AGS_MAP_VIEW = require('esri/views/MapView');
+const AGS_GRAPH_LAYER = require('esri/layers/GraphicsLayer');
+const AGS_LOCATE_WID = require('esri/widgets/Locate');
+const AGS_SEARCH_WID = require('esri/widgets/Search');
+
+@Injectable()
+export class MapService {
+  map: __esri.Map;
+  view: __esri.MapView;
+  graphicsLayer: __esri.GraphicsLayer;
+  locateBtn: __esri.Locate;
+  searchBox: __esri.Search;
+
+  protected _locateEnabled = false;
+  protected _locateOnInit = false;
+
+  initialize(options: MapServiceOptions) {
+    this.initMap(options.basemap);
+    this.initView(options.container, options.maxZoom, options.minZoom, options.rotationEnabled);
+    this.initGraphicsLayer();
+
+    return this;
+  }
+
+  withSearchBox(position = 'top-left', index = 0) {
+    this.searchBox = new AGS_SEARCH_WID({
+      view: this.view
+    });
+    this.view.ui.add(this.searchBox, {
+      position: position,
+      index: index
+    });
+
+    return this;
+  }
+
+  withLocateBtn(position = 'top-left', index = 1) {
+    this.locateBtn = new AGS_LOCATE_WID({
+      view: this.view,
+      graphicsLayer: this.graphicsLayer
+    });
+    this.locateBtn.startup();
+    this.view.ui.add(this.locateBtn, {
+      position: position,
+      index: index
+    });
+    this._locateEnabled = true;
+
+    return this;
+  }
+
+  locateOnInit() {
+    this._locateOnInit = true;
+  }
+
+  protected viewDidInit() {
+    if (this._locateEnabled && this._locateOnInit) this.locateBtn.locate();
+  }
+
+  protected initMap(basemap = 'streets') {
+    this.map = new AGS_MAP({
+      basemap: basemap
+    });
+  }
+
+  protected initView(container: string, maxZoom = 20, minZoom = 2, rotationEnabled = false) {
+    this.view = new AGS_MAP_VIEW({
+      container: container,
+      map: this.map,
+      constraints: {
+        maxZoom: maxZoom,
+        minZoom: minZoom,
+        rotationEnabled: rotationEnabled
+      }
+    });
+
+    this.view.then(() => this.viewDidInit());
+  }
+
+  protected initGraphicsLayer() {
+    this.graphicsLayer = new AGS_GRAPH_LAYER();
+    this.map.add(this.graphicsLayer);
+  }
+}
+
+export interface MapServiceOptions {
+  basemap?: string;
+  container: string;
+  maxZoom?: number;
+  minZoom?: number;
+  rotationEnabled?: boolean;
+}
